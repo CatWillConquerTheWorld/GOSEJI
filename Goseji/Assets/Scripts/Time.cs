cca3741f989baf0d98a8768dc5140f4596b9efac
kunkokouma <sjjk0305@gmail.com>
2024-02-17 22:07:02 +0900

    시간 제작 시작
    시간 그림 좀 추가해놓음

---
+++ Goseji/Assets/Scripts/Time.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    static public string morning1;
+    public static string morning2;
+    public static string afternoon1;
+    public static string afternoon2;
+    public static string night, ev;
+
+
+    public static string TimeTable[];
+    
+    
+}
