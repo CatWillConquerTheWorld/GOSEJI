30fbbe1c1df04287a0cd90e8848995227b2e2094
kunkokouma <sjjk0305@gmail.com>
2024-02-18 00:44:56 +0900

    코드 지움
    내가 쓴 쓰레기 같은 코드 지움

--- Goseji/Assets/Scripts/Time.cs
+++ Goseji/Assets/Scripts/Time.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
-    static public string morning1;
-    public static string morning2;
-    public static string afternoon1;
-    public static string afternoon2;
-    public static string night, ev;
-
-
-    public static string TimeTable[];
-    
     
 }
